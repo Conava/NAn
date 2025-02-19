323c9714a5a97423cf3797bd327a0ff14dc7dee1
Tculhane8879 <tculhane8879@sdsu.edu>
2025-02-19 13:50:53 -0800

    Basic first outline

---
+++ frontend/src/components/DataTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <table>
+      <thead>
+      <tr>
+        <th v-for="column in columns" :key="column">{{ column }}</th>
+      </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+const columns = ["MAC", "SSID", "Signal Strength", "Radio Type", "Band", "Connected Stations", "Channel Utilization", "Authentication", "Encryption"]
+</script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #ddd;
+}
+th {
+  padding: 10px;
+  background-color: #f4f4f4;
+  border: 1px solid #ddd;
+  text-align: left;
+}
+</style>
