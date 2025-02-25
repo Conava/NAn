9abcea93b31d3252306de9913d8c71f2506ea3be
Mason Leavitt <mason.leavitt8@gmail.com>
2025-02-24 19:01:59 -0800

    Added method scanSpecifyInterface
    This method accepts a string to be used as the network interface device, allowing
    the user to specify which WiFi device to use for the scan should there be more than
    one present.

--- backend/src/main/java/SingleWiFiScanWindows.java
+++ backend/src/main/java/SingleWiFiScanWindows.java
@@ -25,4 +25,27 @@ public class SingleWiFiScanWindows {
             return "IOException";
         }
     }
+
+    public static String scanSpecifyInterface(String interfaceName) {
+        StringBuilder output = new StringBuilder();
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("netsh", "wlan", "show", "networks", "interface=" + interfaceName, "mode=bssid");
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n"); // Append each line to the output
+            }
+
+            return output.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Print error details (you can log it instead)
+            return "IOException";
+        }
+    }
+
 }
