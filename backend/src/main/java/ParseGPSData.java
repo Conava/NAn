0cc9b0941ca15cb80d2061e77f713f9c080c95fe
Mason Leavitt <mason.leavitt8@gmail.com>
2025-02-24 21:48:13 -0800

    Added class ParseGPSData
    In progress, no content yet. Conducting Domain research to better understand
    the content of GPS NMEA sentences. This will shape the structure of the Maps
    for each unique NMEA sentence. The class will return a List of JSONObjects
    that can be merged (via another class) with the JSONObjects produced by the
    ParseWiFiDataWindows class.

---
+++ backend/src/main/java/ParseGPSData.java
@@ -0,0 +1,13 @@
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ParseGPSData {
+
+    public void /*List<JSONObject>*/ parseStringToListOfJSON(String gpsData) {
+        return ;
+    }
+
+    public static void main(String[] args) {}
+}
