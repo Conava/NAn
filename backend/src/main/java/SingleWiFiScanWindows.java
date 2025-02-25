a52bb8bf01630121770f32b47bf4122cc1b615a5
Mason Leavitt <mason.leavitt8@gmail.com>
2025-02-24 21:03:55 -0800

    Broke up the previous scan to accommodate overloading.
    The previous scan method was separated into two methods; one that accepts a process
    builder object (runScan) and another that accepts a string (scan). The string
    identifies the WiFi interface to be used, which in turn is included in the process
    builder object passed into runScan. If no string is provided, the method is
    overloaded such that it will pass a different process builder object into runScan
    that omits the specified interface.

--- backend/src/main/java/SingleWiFiScanWindows.java
+++ backend/src/main/java/SingleWiFiScanWindows.java
@@ -4,11 +4,20 @@ import java.io.InputStreamReader;
 
 public class SingleWiFiScanWindows {
 
+    public static String scan(String interfaceName) {
+        return runScan(new ProcessBuilder("netsh", "wlan", "show", "networks", "interface=" + interfaceName, "mode=bssid"));
+    }
+
+    // Overloaded method without interface name
     public static String scan() {
+        return runScan(new ProcessBuilder("netsh", "wlan", "show", "networks", "mode=bssid"));
+    }
+
+    public static String runScan(ProcessBuilder processBuilder) {
         StringBuilder output = new StringBuilder();
 
         try {
-            ProcessBuilder processBuilder = new ProcessBuilder("netsh", "wlan", "show", "networks", "mode=bssid");
+//            ProcessBuilder processBuilder = new ProcessBuilder("netsh", "wlan", "show", "networks", "interface=" + interfaceName, "mode=bssid");
             Process process = processBuilder.start();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -26,26 +35,8 @@ public class SingleWiFiScanWindows {
         }
     }
 
-    public static String scanSpecifyInterface(String interfaceName) {
-        StringBuilder output = new StringBuilder();
-
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder("netsh", "wlan", "show", "networks", "interface=" + interfaceName, "mode=bssid");
-            Process process = processBuilder.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n"); // Append each line to the output
-            }
-
-            return output.toString();
-
-        } catch (IOException e) {
-            e.printStackTrace(); // Print error details (you can log it instead)
-            return "IOException";
-        }
+    public static void main(String[] args) {
+        System.out.println(scan("Wi-Fi"));
     }
 
 }
