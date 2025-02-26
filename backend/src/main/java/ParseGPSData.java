cbbc34f3dc179328d0df37cd4f3e165bf15efe74
Mason Leavitt <mason.leavitt8@gmail.com>
2025-02-25 20:23:12 -0800

    Completed the initial version of ParseGPSData class
    This class consists of a Map of data labels, 3 String arrays, a constructor, one
    method and a main() for testing. The constructor combines the 3 String arrays to
    initialize the Map; this is done in the constructor. The three string arrays are
    in a particular order to enable proper label matching to data reported by the GPS
    device being used, based on standard GPS NMEA sentences. The method takes in a
    string of NMEA GPS data sentences and parses the contained data into a Map of GPS
    data. Duplicate keys result in overwriting existing values, this occurs 6 times
    (utcTime, latitude, northSouth, longitude, eastWest, hdop). The method returns a
    JSON object; the intent is to merge this JSON object with the JSON object
    returned by ParseWiFiDataWindows, resulting in the final JSON Object to be stored
    in the database.

--- backend/src/main/java/ParseGPSData.java
+++ backend/src/main/java/ParseGPSData.java
@@ -1,13 +1,57 @@
 import org.json.JSONObject;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ParseGPSData {
 
-    public void /*List<JSONObject>*/ parseStringToListOfJSON(String gpsData) {
-        return ;
+    private final Map<String, String[]> dataLabels;
+    private static final String[] gpggaSentenceLabels = { //fix data, arranged in order as reported by NMEA sentences
+            "utcTime", "latitude", "northSouth", "longitude", "eastWest", "positionFix",
+            "satelliteCount", "hdop", "altitude", "altitudeUnits", "geoIdSep", "sepUnits",
+            "dgpsAge", "dgpsStationId"
+    };
+    private static final String[] gpgsaSentenceLabels = { //active satellites, arranged in order as reported by NMEA sentences
+            "selectionMode", "fixType", "sat01", "sat02", "sat03", "sat04",
+            "sat05", "sat06", "sat07", "sat08", "sat09", "sat10",
+            "sat11", "sat12", "pdop", "hdop", "vdop"
+    };
+    private static final String[] gprmcSentenceLabels = { //position and time data, arranged in order as reported by NMEA sentences
+            "utcTime", "status", "latitude", "northSouth", "longitude", "eastWest",
+            "spdOverGrnd", "courseOverGrnd", "date", "magVariation", "magDirection", "modeIndicator"
+    };
+    public ParseGPSData() { //constructor to initialize data labels for use in creating the Map holding the GPS data
+        dataLabels = new HashMap<>();
+        dataLabels.put("GPGGA", gpggaSentenceLabels);
+        dataLabels.put("GPGSA", gpgsaSentenceLabels);
+        dataLabels.put("GPRMC", gprmcSentenceLabels);
     }
 
-    public static void main(String[] args) {}
+
+    public JSONObject parseStringToListOfJSON(String gpsData) {
+
+        Map<String, String> gpsDataMap = new HashMap<>();
+        String[] indivNMEAsentences = gpsData.split("\n");
+        for (String sentence : indivNMEAsentences) {
+            sentence = sentence.substring(1, sentence.indexOf('*'));
+            String[] sentenceComponents = sentence.split(",");
+            String keyToUse = sentenceComponents[0].trim();
+
+            String[] labels = this.dataLabels.get(keyToUse);
+            for (int i = 0; i < labels.length; ++i) {
+                gpsDataMap.put(labels[i], sentenceComponents[i+1].trim());
+            }
+        }
+        return new JSONObject(gpsDataMap);
+    }
+
+    public static void main(String[] args) {
+        String gpsData = "$GPGSA,A,3,29,05,18,23,25,15,13,26,20,12,,,1.13,0.79,0.81*04\n" +
+                "$GPRMC,015455.000,A,3244.8110,N,11707.5409,W,0.11,218.56,260225,,,D*71\n" +
+                "$GPGGA,015456.000,3244.8109,N,11707.5409,W,2,10,0.79,128.5,M,-35.8,M,,0000*55";
+
+        ParseGPSData parser = new ParseGPSData();
+        JSONObject ouput = parser.parseStringToListOfJSON(gpsData);
+        System.out.println(ouput.toString(4));
+    }
 }
