18bd21cb58eabfee36ba0f0aca415bec77284369
Mason Leavitt <mason.leavitt8@gmail.com>
2025-03-11 23:57:38 -0700

    Added a .kml and .csv file creation to main() for testing.

--- backend/src/main/java/org/cs250/nan/backend/shell/ContinuousWiFiGPSScanWindows.java
+++ backend/src/main/java/org/cs250/nan/backend/shell/ContinuousWiFiGPSScanWindows.java
@@ -1,6 +1,8 @@
 package org.cs250.nan.backend.shell;
 
 import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -54,9 +56,12 @@ public class ContinuousWiFiGPSScanWindows {
         running.set(false);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ContinuousWiFiGPSScanWindows contScan = new ContinuousWiFiGPSScanWindows();
-        List <JSONObject> jsonWiFiObjects = contScan.scan(4, 8);
+        List <JSONObject> jsonWiFiObjects = contScan.scan(1, 8);
+//        WriteJSONToKML jsonWriter = new WriteJSONToKML();
+        WriteJSONToKML.writeJSONToKML(jsonWiFiObjects, "kmlOutput.kml");
+        WriteJSONToCSV.writeJsonListToCsv(jsonWiFiObjects, "csvOutput.csv");
 //        System.out.println("\n\n\n\n\n");
         for (JSONObject data : jsonWiFiObjects) { //iterate through the JSON objects, printing each
             System.out.println(data.toString(4));
