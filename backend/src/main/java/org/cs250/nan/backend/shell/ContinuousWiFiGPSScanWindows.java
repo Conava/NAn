160e033c5b838a7cb10326ad5508188774504444
Mason Leavitt <mason.leavitt8@gmail.com>
2025-03-12 00:06:36 -0700

    Deleted old commented-out code.

--- backend/src/main/java/org/cs250/nan/backend/shell/ContinuousWiFiGPSScanWindows.java
+++ backend/src/main/java/org/cs250/nan/backend/shell/ContinuousWiFiGPSScanWindows.java
@@ -1,6 +1,8 @@
 package org.cs250.nan.backend.shell;
 
 import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -54,8 +56,11 @@ public class ContinuousWiFiGPSScanWindows {
         running.set(false);
     }
 
+    public static void main(String[] args) throws IOException {
         ContinuousWiFiGPSScanWindows contScan = new ContinuousWiFiGPSScanWindows();
-//        System.out.println("\n\n\n\n\n");
+        List <JSONObject> jsonWiFiObjects = contScan.scan(1, 8);
+        WriteJSONToKML.writeJSONToKML(jsonWiFiObjects, "kmlOutput.kml");
+        WriteJSONToCSV.writeJsonListToCsv(jsonWiFiObjects, "csvOutput.csv");
         for (JSONObject data : jsonWiFiObjects) { //iterate through the JSON objects, printing each
             System.out.println(data.toString(4));
         }
