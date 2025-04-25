757d9a35d73c3668ece06080d8b373deade9dea5
Mason Leavitt <mason.leavitt8@gmail.com>
2025-04-25 15:37:47 -0700

    Commented out for now, not operational.

--- backend/src/main/java/org/cs250/nan/backend/shell/FromFileSaveToMongoDB.java
+++ backend/src/main/java/org/cs250/nan/backend/shell/FromFileSaveToMongoDB.java
@@ -34,21 +34,21 @@ public class FromFileSaveToMongoDB {
         return jsonObjects;
     }
 
-    public static void fromFileSaveToMongoDB(String filePath) {
-
-        // If connected to MongoDB, write Each JSON object from the single scan to MongoDB
-        MongoConnectionChecker checker = SpringContext.getBean(MongoConnectionChecker.class);
-        boolean mongoOk = checker.isConnected();
-
-        if (mongoOk) {
-            List<JSONObject> scans = readJsonFileToList(filePath);
-            SaveToMongoDB mongoSaver = SpringContext.getBean(SaveToMongoDB.class);
-            for (JSONObject scan : scans) {
-                mongoSaver.insertJSONObject(scan);
-            }
-        }
-        else {
-            System.out.println("MongoDB connection failed: " + mongoOk);
-        }
-    }
+//    public static void fromFileSaveToMongoDB(String filePath) {
+//
+//        // If connected to MongoDB, write Each JSON object from the single scan to MongoDB
+//        MongoConnectionChecker checker = SpringContext.getBean(MongoConnectionChecker.class);
+//        boolean mongoOk = checker.isConnected();
+//
+//        if (mongoOk) {
+//            List<JSONObject> scans = readJsonFileToList(filePath);
+//            SaveToMongoDB mongoSaver = SpringContext.getBean(SaveToMongoDB.class);
+//            for (JSONObject scan : scans) {
+//                mongoSaver.insertJSONObject(scan);
+//            }
+//        }
+//        else {
+//            System.out.println("MongoDB connection failed: " + mongoOk);
+//        }
+//    }
 }
