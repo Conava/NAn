04176cf865ccd0a2430f54ba61bdc2d3b34f5a35
Mason Leavitt <mason.leavitt8@gmail.com>
2025-04-25 13:39:20 -0700

    Removed teh DB classes previously added and replaced with the originals. Those other had already been modified ot work with the main backend, but that system is not working properly at this time. This will be the back up. These classes need to be modified to not rely on springboot (probably)

--- backend/src/main/java/org/cs250/nan/backend/shell/MongoConnectionChecker.java
+++ backend/src/main/java/org/cs250/nan/backend/shell/MongoConnectionChecker.java
@@ -1,44 +1,30 @@
-// src/main/java/org/cs250/nan/backend/database/MongoConnectionChecker.java
 package org.cs250.nan.backend.database;
 
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.cs250.nan.backend.config.AppProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 public class MongoConnectionChecker {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MongoConnectionChecker.class);
 
-    private final boolean remoteEnabled;
-    private final MongoClient client;
+    private final boolean connected;
 
-    public MongoConnectionChecker(AppProperties props) {
-        this.remoteEnabled = props.getDb().isRemoteEnabled();
-        if (remoteEnabled) {
-            // create client once
-            this.client = MongoClients.create(props.getDb().getUri());
-        } else {
-            this.client = null;
+    @Autowired
+    public MongoConnectionChecker(MongoClient mongoClient) {
+        boolean connectionStatus;
+        try {
+            mongoClient.getDatabase("wifiData").runCommand(new org.bson.Document("ping", 1));
+            connectionStatus = true;
+            System.out.println("Connected to MongoDB");
+        } catch (MongoException e) {
+            connectionStatus = false;
+            System.err.println("MongoDB connection failed: " + e.getMessage());
         }
+        this.connected = connectionStatus;
     }
 
-    /**
-     * @return true if remoteEnabled and the server is reachable, false otherwise.
-     */
     public boolean isConnected() {
-        if (!remoteEnabled) {
-            return false;
-        }
-        try {
-            // quick ping
-            client.listDatabaseNames().first();
-            return true;
-        } catch (Exception e) {
-            LOGGER.warn("MongoDB ping failed, disabling DB logic", e);
-            return false;
-        }
+        return connected;
     }
 }
